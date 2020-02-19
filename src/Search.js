a9ca0b8ef511739e4fe30274a506b7b8936df827
nugentmichael <nugent.michael@gmail.com>
2020-02-19 09:57:49 -0500

    Code cleanup.

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
 const Search = () => {
-	// API Key: AIzaSyCMSAQDPcK9X64QQnA767szm0JZ1AypDPA
-	// https://www.googleapis.com/customsearch/v1?key=AIzaSyCMSAQDPcK9X64QQnA767szm0JZ1AypDPA&cx=017576662512468239146:omuauf_lfve&q=lectures
-
 	const [results, setResults] = useState([]);
 	const [searchQuery, setSearchQuery] = useState('lectures');
 	const [url, setUrl] = useState(
