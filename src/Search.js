e94518f57f01013ae15f0b76a775d3e7ebe32173
nugentmichael <nugent.michael@gmail.com>
2020-02-24 08:44:09 -0500

    Add search form markup.

--- src/Search.js
+++ src/Search.js
@@ -2,11 +2,28 @@ import React, { useState, useEffect } from 'react';
 
 const Search = () => {
 	const [results, setResults] = useState([]);
-	const [searchQuery, setSearchQuery] = useState('lectures');
+	const [searchQuery, setSearchQuery] = useState('news');
 	const [url, setUrl] = useState(
 		`https://www.googleapis.com/customsearch/v1?key=AIzaSyCMSAQDPcK9X64QQnA767szm0JZ1AypDPA&cx=017576662512468239146:omuauf_lfve&q=${searchQuery}`
 	);
 	const [loading, setLoading] = useState(false);
+
+	const searchForm = () => (
+		<form onSubmit={handleSubmit}>
+			<input
+				type="text"
+				value={searchQuery}
+				onChange={handleChange}
+			></input>
+			<button>Search</button>
+		</form>
+	);
+
+	const handleChange = e => {
+		setSearchQuery(e.target.value);
+	};
+
+	const handleSubmit = () => {};
 
 	const fetchSearch = () => {
 		// Set loading state to true
@@ -25,6 +42,7 @@ const Search = () => {
 
 	return (
 		<div>
+			{searchForm()}
 			<p>Results</p>
 			{results.map((result, index) => (
 				<a key={index} href={result.link}>
