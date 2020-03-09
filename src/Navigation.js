a4aa49baf40000d4a148744c79da6a7f189e1cd4
nugentmichael <nugent.michael@gmail.com>
2020-03-09 08:53:07 -0400

    Revise navigation menu structure to incorporate React Router.

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,4 +1,12 @@
 import React, { useState } from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	useRouteMatch,
+	useParams
+} from 'react-router-dom';
 
 const Nav = () => {
 	const [hamburger, setHamburger] = useState('');
@@ -24,57 +32,12 @@ const Nav = () => {
 				</div>
 			</div>
 
-			<ul className="list-unstyled components">
-				<li className="active">
-					<a
-						href="#homeSubmenu"
-						data-toggle="collapse"
-						aria-expanded="false"
-						className="dropdown-toggle"
-					>
-						Home
-					</a>
-					<ul className="collapse list-unstyled" id="homeSubmenu">
-						<li>
-							<a href="#">Home 1</a>
-						</li>
-						<li>
-							<a href="#">Home 2</a>
-						</li>
-						<li>
-							<a href="#">Home 3</a>
-						</li>
-					</ul>
+			<ul className="menu-components">
+				<li>
+					<Link to="/Search">Search</Link>
 				</li>
 				<li>
-					<a href="#">About</a>
-				</li>
-				<li>
-					<a
-						href="#pageSubmenu"
-						data-toggle="collapse"
-						aria-expanded="false"
-						className="dropdown-toggle"
-					>
-						Pages
-					</a>
-					<ul className="collapse list-unstyled" id="pageSubmenu">
-						<li>
-							<a href="#">Page 1</a>
-						</li>
-						<li>
-							<a href="#">Page 2</a>
-						</li>
-						<li>
-							<a href="#">Page 3</a>
-						</li>
-					</ul>
-				</li>
-				<li>
-					<a href="#">Portfolio</a>
-				</li>
-				<li>
-					<a href="#">Contact</a>
+					<Link to="/Weather">Weather</Link>
 				</li>
 			</ul>
 		</nav>
