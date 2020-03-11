eb04762e5221fbd187b51b49dde8ee5f7cee86a0
nugentmichael <nugent.michael@gmail.com>
2020-03-11 10:15:16 -0400

    Configure weather API fetch base functionality.

--- src/Weather.js
+++ src/Weather.js
@@ -3,23 +3,47 @@ import keys from './keys';
 
 const Weather = () => {
 	const [ip, setIP] = useState('https://www.cloudflare.com/cdn-cgi/trace');
-	const [latLng, setlatLng] = useState(
-		`http://api.ipstack.com/108.162.130.190?access_key=${keys.ipstack}&format=1`
-	);
-	const [weather, setWeather] = useState();
+	const [latLng, setLatLng] = useState([]);
+	const [weather, setWeather] = useState([]);
 
-	const fetchIP = () => {
-		fetch(latLng)
+	const fetchLatLng = () => {
+		fetch(
+			`http://api.ipstack.com/108.162.130.190?access_key=${keys.ipstack}&format=1`
+		)
 			.then(result => result.json())
-			// .then(data => console.log(data));
-			.then(data => setIP(data.ip));
+			// .then(data => console.log(data))
+			.then(data => setLatLng(data))
+			.catch(error => console.log(error));
 	};
 
 	useEffect(() => {
-		fetchIP();
-	});
+		if (latLng) {
+			fetchLatLng();
+		}
 
-	return <h2>Weather</h2>;
+		if (weather) {
+			// fetchWeather();
+		}
+	}, [latLng, weather]);
+
+	const fetchWeather = () => {
+		fetch(
+			`https://api.openweathermap.org/data/2.5/weather?lat=${latLng.latitude}&lon=${latLng.longitude}&appid=${keys.weather}`
+		)
+			.then(result => result.json())
+			.then(data => console.log(data));
+		// .then(data => setWeather(data));
+	};
+
+	return (
+		<div>
+			<h2>Weather</h2>
+			<div>
+				<h3>{latLng.latitude}</h3>
+				<p>{latLng.longitude}</p>
+			</div>
+		</div>
+	);
 };
 
 export default Weather;
