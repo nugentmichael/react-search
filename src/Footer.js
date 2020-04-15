dc0ba4158661b3ab0d869f46a6cbf5845601c827
nugentmichael <nugent.michael@gmail.com>
2020-04-15 09:50:37 -0400

    Style and markup revisions to the Header and Footer components.

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<footer className="App-footer">
-			Roogle: A React Search App &copy; {new Date().getFullYear()}
+			<h6>
+				<strong>Roogle:</strong> A React Search App &copy;{' '}
+				{new Date().getFullYear()}
+			</h6>
 		</footer>
 	);
 };
